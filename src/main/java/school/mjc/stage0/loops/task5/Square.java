37ff2018098fd068c6c683fac9acc839504e2542
Avazbek Isroilov <64166521+Avazbek2002@users.noreply.github.com>
2022-08-02 03:43:27 +0500

    loop

--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -1,7 +1,25 @@
 package school.mjc.stage0.loops.task5;
 
 public class Square {
-    public void printSquareFrom8s(int sideLength){
+    public static void printSquareFrom8s(int sideLength){
+        for (int i = 0; i < sideLength; i++) {
+            if (i == 0 || i == sideLength - 1) {
+                for (int j = 0; j < sideLength; j++) {
+                    System.out.print(8);
+                }
+                System.out.println();
+            }
+            else {
+                System.out.print(8);
+                for (int j = 1; j < sideLength - 1; j++) {
+                    System.out.print(" ");
+                }
+                System.out.println(8);
+            }
+        }
+    }
 
+    public static void main (String[] args) {
+        printSquareFrom8s(5);
     }
 }
